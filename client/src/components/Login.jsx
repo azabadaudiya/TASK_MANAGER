4bfa96da5b75c6104a3afdc3a9160dfad63bc0cb
azabadaudiya <146463403+azabadaudiya@users.noreply.github.com>
2024-03-08 17:20:31 +0530

    Update Login.jsx

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -14,7 +14,7 @@ function Login({ isAuthenticated, setIsAuthenticated }) {
     e.preventDefault();
     axios
       .post(
-        "http://localhost:4000/api/v1/user/login",
+        "https://wicked-singlet-tick.cyclic.app/api/v1/user/login",
         { email, password },
         {
           withCredentials: true,
